610c8601cfe0c7c0b17d24ab981924af2cb000a8
NureTarasovRostyslav <rostyslav.tarasov@nure.ua>
2024-10-15 16:02:48 +0300

    practice 1.2 ПЗ Створення мінімального Android застосунку

--- PractTask1/android-pzpi-23-8-tarasov-rostyslav-pract-task1/app/src/main/java/com/nure_ua_tarasov/practice1/MainActivity.java
+++ PractTask1/android-pzpi-23-8-tarasov-rostyslav-pract-task1/app/src/main/java/com/nure_ua_tarasov/practice1/MainActivity.java
@@ -1,6 +1,8 @@
 package com.nure_ua_tarasov.practice1;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +11,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        findViewById(R.id.change_counter_button).setOnClickListener(this::add1);
+    }
+
+    private void add1(View view) {
+        TextView tv = findViewById(R.id.text_view_counter);
+
+        counter++;
+        tv.setText(String.valueOf(counter));
     }
 }
