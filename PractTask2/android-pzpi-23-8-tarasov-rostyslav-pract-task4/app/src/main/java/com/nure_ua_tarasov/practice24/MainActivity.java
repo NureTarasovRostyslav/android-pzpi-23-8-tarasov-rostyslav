f69b99421cdf5e5224605a64034c59b4ef1d669d
NureTarasovRostyslav <rostyslav.tarasov@nure.ua>
2024-10-15 19:21:33 +0300

    Файли розмітки Layout. Самостійне завдання

---
+++ PractTask2/android-pzpi-23-8-tarasov-rostyslav-pract-task4/app/src/main/java/com/nure_ua_tarasov/practice24/MainActivity.java
@@ -0,0 +1,99 @@
+package com.nure_ua_tarasov.practice24;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textViewResult;
+    private StringBuilder currentInput = new StringBuilder();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        textViewResult = findViewById(R.id.displayTextView);
+
+        setButtonListeners();
+    }
+
+    private void setButtonListeners() {
+        int[] buttonIds = {
+                R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+                R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
+                R.id.buttonPlus, R.id.buttonMinus, R.id.buttonMultiply, R.id.buttonDivide,
+                R.id.buttonEquals, R.id.buttonBackspace
+        };
+
+        for (int id : buttonIds) {
+            findViewById(id).setOnClickListener(this::onButtonClick);
+        }
+    }
+    private void onButtonClick(View view) {
+        Button button = (Button) view;
+        String buttonText = button.getText().toString();
+
+        if (buttonText.equals("⌫")) {
+            backspace();
+        } else {
+            currentInput.append(buttonText);
+            textViewResult.setText(currentInput.toString());
+        }
+    }
+
+    private void backspace() {
+        if (currentInput.length() > 0) {
+            currentInput.deleteCharAt(currentInput.length() - 1);
+            textViewResult.setText(currentInput.toString());
+        }
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onRestart() {
+
+        super.onRestart();
+    }
+}
