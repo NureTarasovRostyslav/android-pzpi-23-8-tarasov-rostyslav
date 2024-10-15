b9e1a59d0c05441d453ccd1c10e3856827003f5d
NureTarasovRostyslav <rostyslav.tarasov@nure.ua>
2024-10-15 18:18:23 +0300

    Файли розмітки Layout. Додаткові завдання

--- PractTask2/android-pzpi-23-8-tarasov-rostyslav-pract-task3/app/src/main/java/com/nure_ua_tarasov/practice23/MainActivity.java
+++ PractTask2/android-pzpi-23-8-tarasov-rostyslav-pract-task3/app/src/main/java/com/nure_ua_tarasov/practice23/MainActivity.java
@@ -1,6 +1,9 @@
 package com.nure_ua_tarasov.practice23;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +12,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button registerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +26,24 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        usernameEditText = findViewById(R.id.usernameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        registerButton = findViewById(R.id.registerButton);
+
+        registerButton.setOnClickListener(v -> {
+            String username = usernameEditText.getText().toString();
+            String email = emailEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+
+            if (username.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(MainActivity.this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+            } else {
+                // Handle registration logic here
+                Toast.makeText(MainActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
