27f780736ebaaa2154dee9781b999beae7981b84
NureTarasovRostyslav <rostyslav.tarasov@nure.ua>
2024-10-15 17:09:46 +0300

    ПЗ Розмітка екрана

--- PractTask2/android-pzpi-23-8-tarasov-rostyslav-pract-task2/app/src/main/java/com/nure_ua_tarasov/practice22/MainActivity.java
+++ PractTask2/android-pzpi-23-8-tarasov-rostyslav-pract-task2/app/src/main/java/com/nure_ua_tarasov/practice22/MainActivity.java
@@ -1,6 +1,10 @@
 package com.nure_ua_tarasov.practice22;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void btn_click(View view) {
+        if (view instanceof Button) {
+            TextView output = findViewById(R.id.textView0);
+            String old = output.getText().toString();
+
+            if (((Button) view).getText().equals("backspace")){
+                output.setText(removeLastChar(old));
+            }else{
+                output.setText(old+((Button) view).getText());
+            }
+
+        }
+    }
+
+    public static String removeLastChar(String s) {
+        return (s == null || s.isEmpty())
+                ? null
+                : (s.substring(0, s.length() - 1));
     }
 }
