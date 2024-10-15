cd915a86b855570f0605920dd7cbb34a0eab3f74
NureTarasovRostyslav <rostyslav.tarasov@nure.ua>
2024-10-15 13:15:32 +0300

    Init commit

---
+++ PractTask1/android-pzpi-23-8-tarasov-rostyslav-pract-task1/app/src/main/java/com/nure_ua_tarasov/practtask1/MainActivity.java
@@ -0,0 +1,36 @@
+package com.nure_ua_tarasov.practtask1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int counter = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        findViewById(R.id.add1_button).setOnClickListener(this::add1);
+    }
+
+    public void add1(View view) {
+        TextView tv = findViewById(R.id.counter_textView);
+
+        counter++;
+        tv.setText(String.valueOf(counter));
+    }
+}
